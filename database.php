9094b6d3c431c9e31f50584ef75d07279794c89b
xRubenCamargox <rubenartemiosaravia70@hotmail.com>
2025-05-18 10:09:31 -0400

    conexion con la base de datos

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
     require_once "connection.php";
 
     function enlace(){
-        $link=new Conexion("localhost","root","","prueba");
+        $link=new Conexion("bd9jn9xxhlu4pswg8bk9-mysql.services.clever-cloud.com","uwyh2sicpishub5z","FXUcV3yNwgl38OEIz7PA","bd9jn9xxhlu4pswg8bk9");
         return $link->conectar();       
     }
 
